a16741c7ee1a8c243e805c72760476c5a2179c10
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-09-18 18:59:02 -0700

    added dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create([
+  {name: 'Grant', genera: 'RPG', game: 'Breath of the Wild', gender: 'M', hours: 'Pasific'},
+  {name: 'Lucy', genera: 'RPG', game: 'Skyrim', gender: 'F', hours: 'Eastern'},
+  {name: 'Keith', genera: 'Strategy', game: 'XCOM 2', gender: 'M', hours: 'Pasific'},
+  {name: 'Suzy', genera: 'Casual', game: 'Animal Crossing', gender: 'F', hours: 'Pasific'},
+])
