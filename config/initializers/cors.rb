c16bf3bce09f3ff75676261ffdccd61789d2a43c
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-09-26 21:24:08 -0700

    fixed cors again lol

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://dating-for-gamers.herokuapp.com','localhost: 2080']
+    origins ['https://dating-for-gamers.herokuapp.com','localhost:2080']
 
     resource '*',
       headers: :any,
